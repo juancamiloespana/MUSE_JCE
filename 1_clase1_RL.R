65ed9bf49bc8c2a61f511be47fdd5a89cb1a99cd
Juan C. Espana L <camilo1260@gmail.com>
2022-05-27 15:50:17 -0500

    carga inicial

---
+++ 1_clase1_RL.R
@@ -0,0 +1,30 @@
+
+set.seed(100) ###semilla
+t=c(1:200) ##Consecutivo del tiempo
+pib=200+ 4*t + rnorm(n=200,mean=100,sd=50) ###Crear variable ficticia
+
+pib_ts=ts(pib, start=1820,frequency =1 ) ##Convertir variable a serie de tiempo
+plot(pib_ts) ### para grficar
+
+print(pib_ts) ###Para ver información de la serie
+
+n=length(pib_ts)
+
+# propiedades de la serie de tiempo
+
+start(pib_ts)
+end(pib_ts)
+frequency(pib_ts)
+
+### Ajustar modelo de regresión lineal
+m1=lm(pib_ts~t)
+summary(m1)
+
+#######Ejercicio ###
+
+set.seed(100) ###semilla
+t=c(1:200) ##Consecutivo del tiempo
+pib=200+ 4*t + 2*t^2 + rnorm(n=200,mean=100,sd=50)
+  
+mean(pib) ### para verificar valor
+
