f6fa59eb5edc5ede7aa55886c257f8274a3d35df
juancamiloespana <camilo1260@gmail.com>
2023-05-09 07:35:30 -0500

    cambio sarima

---
+++ 010_ejercicio_SARIMA_completo.R
@@ -0,0 +1,12 @@
+
+
+###### Ajuste un modelo para la serie  de producción de electricidad mensual en australia
+###### amep.csv  que se encuentra en la carpeta data de github
+##### la serie inicia en enero de 1956 
+
+#### Realice un pronostico para agosto de 1997
+
+#### cuál sería su pronóstico si tuviera que asegurar que solo se arriesga un
+#### 3% a tener un valor real superior a lo pronósticado
+
+
