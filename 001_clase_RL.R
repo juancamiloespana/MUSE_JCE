a875d5f9d5ae99d51396e4dc164cad445c6771ca
juancamiloespana <camilo1260@gmail.com>
2024-08-08 14:34:04 -0500

    primera clase 2024

---
+++ 001_clase_RL.R
@@ -0,0 +1,38 @@
+##### cargar paquetes ##########
+
+
+
+#### 1. Leer tabla desde github ###
+
+
+#### 2. Convertir a formato ts ####
+
+
+### 3. graficar la serie y analizarla
+
+
+
+#### 4. crear modelo de RL y analizarlo
+
+##### 4.1 crear variable tiempo y crear data frame ###
+
+
+#### 4.2 probar que la serie y el tiempo tengan la misma longitud
+
+
+#### 4.3 Ajustar modelo RL
+
+###  4.4 Generar resumen del modelo y analizarlo
+
+###  5. Escribir modelo con base en salida
+
+##  6. Graficar modelo sobre la serie 
+
+##  7. Hacer predicciones de 2024 del PIB
+
+### 8. COmparar valor ajustado 1850 con valor real 
+
+### 9. Validar supuesto
+
+
+
